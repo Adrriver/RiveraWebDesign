2fbbd8b77eab657eae5f2d2cfa5d043ad3388412
Adrian M. Rivera <AdrianMRivera@macbook-pro.home>
2015-07-02 22:12:36 -0600

    menu has been started; needs wrapping solution

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Rivera Web Design</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+	<link href="css/_mystyles.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <nav class="navbar navbar-default navbar-fixed-top" style="border-radius: 0; height: 75px">
+    	<div id="fixedNav" class="container-fluid" >
+    		<div class="navbar-header" style="width:100%;!important">
+    			<a class="navbar-brand" href="#" style="padding-top: 25px; padding-left: 25px">
+    				<img alt="Brand" src"">
+    			</a>    		
+    				<ul class="navbar-nav navbar-right" style="white-space: nowrap;!important">
+    					<li class="navbar-brand" style="border-bottom-left-radius: 0px; width:100px"><img alt="logo" src""></li>
+    					<li><a href="#"><h4>Home</h4></a></li>
+    					<li><a href="#"><h4>Portfolio</h4></a</li>
+    					<li><a href="#"><h4>Design Services</h4></a></li>
+    					<li><a href="#"><h4>Development Services</h4></a></li>
+    					<li><a href="#"><h4>Pricing</h4></a></li>
+    				</ul>
+    			</nav>
+    		</div>
+    	</div>    				
+	</nav>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
