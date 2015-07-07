7b6f6c6da9cfee912de85b7737e860ed6e384b18
Adrriver <Adrriver@nmsu.edu>
2015-07-07 08:06:52 -0600

    re-init

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Rivera Web Design</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+	<link href="css/_mystyles.css" rel="stylesheet">
+	<!-- Bootstrap CDN -->
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+<<<<<<< HEAD
+    <nav class="navbar navbar-default navbar-fixed-top" style="border-radius: 0; height: 75px; background: #D9F3FF;">
+=======
+    <nav class="navbar navbar-default navbar-fixed-top" style="border-radius: 0; height: 0px">
+>>>>>>> a79477ee64907cf501b2da92150fbf99e0592513
+    	<div id="fixedNav" class="container-fluid" >
+    		<div class="navbar-header" style="width:100%;!important">
+    			<a id="brand" class="navbar-brand" href="#" style="padding-top: 25px; padding-left: 25px;!important">
+    				<img alt="Brand" src"">
+<<<<<<< HEAD
+    			</a>    		    				
+    			<button id="main-search" class="navbar-brand" style="border-bottom-left-radius: 0px; height: 74px;" data-target="#"><span class="glyphicon glyphicon-search" style="padding-top:10px"></span></button>
+    				<form>
+  						<div class="form-group">    					
+    						<input type="search" class="form-control" id="search" placeholder="search">
+  					</div>
+  					</form>
+    				<ul class="navbar-nav navbar-right" style="white-space: nowrap;!important">    					    						
+=======
+    			</a>    		
+    				<div style="display:inline-block"></div>
+    				<ul class="navbar-nav navbar-right" style="white-space: nowrap;!important">
+    					<li id="main-search" class="navbar-brand" style="border-bottom-left-radius: 0px;">
+    						<a data-target="#"><span class="glyphicon glyphicon-search"></span><h4>Search</h4></a>
+    					</li>
+>>>>>>> a79477ee64907cf501b2da92150fbf99e0592513
+    					<li><a href="#"><h4>Home</h4></a></li>
+    					<li><a href="#"><h4>Portfolio</h4></a</li>
+    					<li><a href="#"><h4>Design Services</h4></a></li>
+    					<li><a href="#"><h4>Development Services</h4></a></li>
+    					<li><a href="#"><h4>Pricing</h4></a></li>
+    				</ul>
+<<<<<<< HEAD
+    			
+    		</div>
+    	</div>    				
+	</nav>
+	
+		<div>
+		<h1>What is happening?</h1>
+		</div>
+	
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap-min.js"></script>
+    <script src="js/myScripts-min.js"></script>
+=======
+    			</nav>
+    		</div>
+    	</div>    				
+	</nav>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+>>>>>>> a79477ee64907cf501b2da92150fbf99e0592513
+  </body>
+</html>
